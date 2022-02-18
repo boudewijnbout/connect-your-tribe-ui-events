ec5c63ff2e96ad89edf6886b9eacb686fa1aef22
boudewijnbout <boudewijn_bout@outlook.com>
2022-02-18 10:37:51 +0100

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+// Define Elements
+const buttons = document.querySelectorAll('a');
+
+// Loop trough all buttons
+buttons.forEach(button => {
+
+    // Add single click event listener
+    button.addEventListener('click', function() {
+        button.classList.add('color-green');  
+    });
+
+    // Add double click event listener
+    button.addEventListener('dblclick', function() { 
+        button.classList.add('color-purple');
+    });
+
+    // Add keydown event listener
+    button.addEventListener('keydown', function(e) {
+        if (e.code == 'KeyZ') {
+            button.classList.add('color-lightpurple');
+        }
+     });
+});
